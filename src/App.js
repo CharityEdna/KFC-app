320851e40a9cbac661e2a82ed68e764edfa8da8d
CharityEdna <136688115+CharityEdna@users.noreply.github.com>
2023-12-01 16:06:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { AllRouters } from "./AllPages/AllRouters";
+
+
+import { Navbar } from "./AllPages/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <br/>
+      <br/>
+      {/* <br/> */}
+      
+      <AllRouters/>
+    </div>
+  );
+}
+
+export default App;
